ce1a0e90175e3641f96bb07d3ca31aa86e141879
Hitamashi <dothanhquocviet@yahoo.com.vn>
2021-03-23 17:54:05 +0700

    Deploy to prod (#14)
    * Update travis-ci
    
    * Update travis-ci
    
    * Update travis-ci
    
    * Update travis-ci
    
    * Update travis-ci
    
    * Fix env var in deploy script
    
    * Update travis-ci
    
    * Update README.md
    
    * Update deploy.sh (#6)
    
    gcp cloud function deploy with runtime
    
    * Update deployment script (#7) (#9)
    
    * Update travis-ci
    
    * Update travis-ci
    
    * Update travis-ci
    
    * Update travis-ci
    
    * Update travis-ci
    
    * Fix env var in deploy script
    
    * Update travis-ci
    
    * Update README.md
    
    * Update deploy.sh (#6)
    
    gcp cloud function deploy with runtime
    
    * Bumps flask from 0.12.3 to 1.1.1 (#10)
    
    * Migrate runtime to python37 (#12)
    
    * Migrate GAE to python3
    
    * Try using cloud build for deployment
    
    * Fix cloud build deployment
    
    * Remove threadsafe in app.yaml
    
    * Edit scirpt to auto in app.yaml
    
    * Feature/move to python client api (#13)
    
    * Using python client api to list gce instances
    
    * Remove travis ci
    
    * Cleanup code
    - Remove cron
    - Refactor code
    
    * Update deploy script
    - Remove Cloud function
    - Cleanup unused code
    
    * Add substitutions in cloudbuild.yaml

--- deploy.sh
+++ deploy.sh
@@ -1,50 +1,39 @@
-# Deploy Cloud function
-# Accept params:
-#   - $1 Cloud function name
-#   - $2 function name in file
-#   - $3 region name
-#   - $4 memory
-deployCF() {
-	echo "Deploy Cloud Function $1 in $3 region"
-	gcloud functions deploy $1 \
-  	--source https://source.developers.google.com/projects/${PROJECTID}/repos/gce-compute/moveable-aliases/master/paths/cloudfunction \
-  	--trigger-http --entry-point=$2 --region=$3 --memory=$4 --runtime nodejs6
-}
+#!/bin/bash
 
 configGenerate() {
 	cp app.yaml app-deploy.yaml
-	echo "env_variables:" >> app-deploy.yaml
-	echo "  GCP_CLOUDFUNCTION_URL: \"$GCP_CLOUDFUNCTION_URL\"" >> app-deploy.yaml
-	echo "  GCP_DEFAULT_ZONE: \"$GCP_DEFAULT_ZONE\"" >> app-deploy.yaml
-	echo "  GCP_INSTANCE_NAME: \"$GCP_INSTANCE_NAME\"" >> app-deploy.yaml
+	[[ ! -z $GOOGLE_APPLICATION_CREDENTIALS ]] && echo "  GOOGLE_APPLICATION_CREDENTIALS: \"$GOOGLE_APPLICATION_CREDENTIALS\"" >> app-deploy.yaml
+
+	echo "Config generated!"
 }
 
 remote() {
-	PROJECTID=`gcloud projects list | grep -iw "$1" | awk '{print $1}'`
-
-	if [ -z "$PROJECTID" ]; then
-	 echo Project $1 Not Found!
-	 exit
+	if gcloud projects describe "$PROJECT_ID" 2>&1 > /dev/null; then
+		echo "Project ID $PROJECT_ID"
+	else
+		echo Project $1 Not Found!
+		exit 1
 	fi
-
-	echo "Project ID $PROJECTID"
-	gcloud config set project $PROJECTID
-
-	echo "Deploy Cloud function"
-
-	deployCF getInstance getInstance asia-northeast1 128MB
-	deployCF startInstance startInstance asia-northeast1 128MB
-	deployCF stopInstance stopInstance asia-northeast1 128MB
 
 	echo "Deploy App engine & Cron job"
 	configGenerate
-	gcloud app deploy -q app-deploy.yaml cron.yaml
+
+	_GAE_VERSION=${2:-test}
+	_GAE_PROMOTE=${3:---no-promote}
+
+	echo "[ENV] $_GAE_VERSION"
+	echo "[CMD] cloud app deploy --project $PROJECT_ID -q app-deploy.yaml cron.yaml $_GAE_PROMOTE -v $_GAE_VERSION"
+	gcloud app deploy --project $PROJECT_ID \
+		-q app-deploy.yaml cron.yaml \
+		$_GAE_PROMOTE \
+		-v $_GAE_VERSION
 }
 
 local() {
 	echo "Run app on local machine, port $1"
+	export GOOGLE_APPLICATION_CREDENTIALS=./gcp-secret.json
 	configGenerate
-	dev_appserver.py app-deploy.yaml --port=$1
+	dev_appserver.py app-deploy.yaml --port=$1 --log_level=debug --application=composite-drive-196403
 }
 
 usage() {
@@ -54,6 +43,7 @@ usage() {
     echo "-----------Parameter-------------"
     echo "local : Deploy app in local env"
     echo "remote: Deploy app in GAE (need project id)"
+	echo "config: Generate config file app-deploy.yaml"
 }
 
 case "$1" in
@@ -65,12 +55,11 @@ case "$1" in
 		fi
 		;;
     remote)
-		if [[ $# -eq 2 ]] ; then
-			remote $2
-		else
-			echo "Missing project id"
-			exit 1
-		fi
+		remote
+		;;
+	config)
+		echo "Generate config file in app-deploy.yaml"
+		configGenerate
 		;;
     *) usage ;;
 esac
--- main.py
+++ main.py
@@ -1,62 +1,89 @@
 # [START app]
 import logging
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # [START imports]
 import requests
-import requests_toolbelt.adapters.appengine
 import json
 import os
 from urllib3.exceptions import HTTPError
+from googleapiclient.discovery import build
 
-# Use the App Engine Requests adapter. This makes sure that Requests uses
-# URLFetch.
-requests_toolbelt.adapters.appengine.monkeypatch()
 # [END imports]
 
 app = Flask(__name__)
 
-zone = os.getenv('GCP_DEFAULT_ZONE')
-vm = os.getenv('GCP_INSTANCE_NAME')
-url_endpoint = os.getenv('GCP_CLOUDFUNCTION_URL')
-
-
 @app.route('/')
-def index():
+def list():
     # [START requests_start]
-    url = url_endpoint + '/getInstance'
     try:
-        response = requests.get(url, params={'zone': zone, 'vm': vm})
-        response.raise_for_status()
-        instance = json.loads(response.text)
-        instance["staticIP"] = instance["networkInterfaces"][0]["accessConfigs"][0]["natIP"]
-        return render_template("index.html", ins=instance)
+        compute = build('compute', 'v1', cache_discovery=False)
+        params = {
+            'project': os.getenv('GOOGLE_CLOUD_PROJECT'),
+            'fields': 'items/*/instances(id,name,status,labels,networkInterfaces/accessConfigs/natIP)'
+        }
+
+        instances = []
+        request = compute.instances().aggregatedList(**params) # pylint: disable=E1101
+        while request is not None:
+            response = request.execute()
+
+            for name, instances_scoped_list in response['items'].items():
+                logging.debug("{}\n{}".format(name,instances_scoped_list))
+                for i in instances_scoped_list['instances']:
+                    i['zone'] = name.split('/')[-1]
+                    try:
+                        i['IP'] = i['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+                    except Exception as ex:
+                        i['IP'] = '-'
+                    try:
+                        i['function'] = i['labels']['function']
+                    except Exception as ex:
+                        i['function'] = '-'
+                instances = instances + instances_scoped_list.get('instances', [])
+
+            request = compute.instances().aggregatedList_next(previous_request=request, previous_response=response)  # pylint: disable=E1101
+        return render_template("index.html", instances=instances)
     except ValueError:
-        return "Cannot get ts info"
-    except HTTPError:
-        return "Request error"
+        return "Cannot get servers info"
+    except HTTPError as ex:
+        logging.exception(ex)
+        return "Request error", 500
 
     # [END requests_start]
-
 
 @app.route('/startTS')
 def startTS():
     # [START requests_get]
-    url = url_endpoint + '/startInstance'
-    response = requests.get(url, params={'zone': zone, 'vm': vm})
-    response.raise_for_status()
-    return response.text
+    compute = build('compute', 'v1', cache_discovery=False)
+    params = {
+        'project': os.getenv('GOOGLE_CLOUD_PROJECT'),
+        'zone': request.args.get('zone'),
+        'instance': request.args.get('vm')
+    }
+    res = compute.instances().start(**params).execute() # pylint: disable=E1101
+    if 'error' in res:
+        logging.debug(res['error'])
+        return "Error occored!", 500
+    return "", 200
     # [END requests_get]
 
 
 @app.route('/stopTS')
 def stopTS():
     # [END requests_stop]
-    url = url_endpoint + '/stopInstance'
-    response = requests.get(url, params={'zone': zone, 'vm': vm})
-    response.raise_for_status()
-    return response.text
+    compute = build('compute', 'v1', cache_discovery=False)
+    params = {
+        'project': os.getenv('GOOGLE_CLOUD_PROJECT'),
+        'zone': request.args.get('zone'),
+        'instance': request.args.get('vm')
+    }
+    res = compute.instances().start(**params).execute()
+    if 'error' in res:
+        logging.debug(res['error'])
+        return "Error occored!", 500
+    return "", 200
     # [END requests_stop]
 
 
